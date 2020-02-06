56cc9aa0a21a8461e9adeae9856157c2b49744cf
Muj04 <53007890+Muj04@users.noreply.github.com>
2020-02-06 13:50:57 +0200

    Add files via upload

---
+++ KingJames.js
@@ -0,0 +1,134 @@
+
+
+
+// body{
+// 	background-color: 	#201728;
+// }
+// .image1 {
+// 	width: 100%;
+// }
+// /* Container holding the image and the text */
+// .container {
+// 	position: relative;
+// 	text-align: center;
+// 	color: white;
+
+// }
+
+// /* Centered text */
+// .centered {
+//   position: absolute;
+//   top: 40%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+ 
+// }
+// .para1 {
+// 	font-size: 20px;
+// 	padding: 20px;
+// 	padding-left: 50px;
+// 	padding-right: 50px;
+// 	background-color: #8600b3;
+// 	color: white;
+// 	font-family: 'Playfair Display', serif;
+// }
+
+/*.div2 {
+	position: absolute;
+	padding-left: 5px;
+	padding-right: 5px;
+	 top: 90%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+ /* transform: skewY(-5deg);
+ }
+
+}*/
+// .div2{
+//   background: #8600b3;
+//   color: #fff;
+//   position: absolute;
+//   z-index: 1;
+//   left: 50%;
+//   top: 100%;
+//    transform: translate(-50%, -50%);
+// }
+// .div2:after {
+//   background: inherit;
+//   bottom: 0;
+//   content: '';
+//   display: block;
+//   height: 50%;
+//   left: 0;
+//   position: absolute;
+//   right: 0;
+//   transform: skewY(-1.5deg);
+//   transform-origin: 100%;
+//   z-index: -1;
+// }
+
+
+// button {
+// 	 border: 2px solid;
+// 	 border-color: red;
+// 	 padding: 10px;
+// 	 padding-left: 10px;
+// 	 padding-right: 10px;
+// 	 background-color: #8600b3;
+// 	  font-family: 'Montserrat', sans-serif;
+
+// }
+
+// a {
+// 	text-decoration: none;
+// 		 color: white;
+// 		 padding-left: 20px;
+// 		 padding-right: 20px;
+// 		 opacity: 0.5;
+// }
+
+// .arrow-1 {
+// 	position: absolute;
+// 	right: 147px;
+// 	top: 185px;
+// }
+
+// @media only screen and (min-width:1000px) {
+//   /* For mobile phones: */
+// .arrow-1 {
+	
+// 	right: 34%;
+// 	top: 70%;
+// }
+/*.section2 {
+	position: relative;
+top: 100%;
+left:20%;
+}*/
+// .hex{
+// 	position: absolute;
+// 	top: 70%;
+// 	left: 50%;
+// }
+// }
+
+// .arrow2{
+// position: absolute;
+// top: 43%;
+// left:50%;
+// }
+
+// .hex{
+// 	position: absolute;
+// 	top: 50%;
+// 	right: 40%;
+// }
+
+// .slider1 {
+// 	position: absolute;
+// 	top: 55%;
+// 	left: 40%;
+// 	width: 45%;
+// 	height: 30%;
+// 	z-index: 2;
+// }
